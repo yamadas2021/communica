e9c4ef399174cb66f6a7ad751bea6f528dd7e428
yamadas2021 <89445915+yamadas2021@users.noreply.github.com>
2022-01-02 16:58:02 +0900

    Add files via upload

---
+++ function.js
@@ -0,0 +1,7 @@
+// The function here takes the parameters that you
+// have declared in the `glide.json` file, in the
+// same order.
+
+window.function = function (no, hash) {
+  return "test score";
+}
